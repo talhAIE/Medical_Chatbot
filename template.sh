84b58c85639b182e87ec7090c34bdf98ab0aa853
Talha Abbasi <talhabbasiaii@gmail.com>
2025-09-01 17:43:07 +0500

    project template add

---
+++ template.sh
@@ -0,0 +1,17 @@
+# make directories
+
+mkdir -p src
+mkdir -p research
+
+# creating files
+
+touch src/__init__.py # to make src a package
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
